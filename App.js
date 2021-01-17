ddcd9c7e068a9cad04ed3ce7aebc06a87833d201
Anil <anil_gurung15@yahoo.com>
2021-01-16 17:22:33 -0800

    added TouchableWithoutFeedback

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
-      <Image source = {{
-        width: 200,
-        height: 200,
-        uri: "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHw%3D&auto=format&fit=crop&w=600&q=60"
-      }}
-      />
+      <TouchableWithoutFeedback onPress={() => console.log("image tapped")}
+        <Image source = {{
+          width: 200,
+          height: 200,
+          uri: "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHw%3D&auto=format&fit=crop&w=600&q=60"
+        }}
+        />
+
+      </TouchableWithoutFeedback>
     </View>
   );
 }
